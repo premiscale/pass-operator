9243df08c7b78b48eeb3b7e25b09329fcb53ea49
Emma Doyle <emma@sbevision.com>
2023-06-12 18:19:41 -0400

    ENG-196: fix some things

--- README.md
+++
@@ -1,3 +0,0 @@
-# Pass kubernetes operator
-
-This Kubernetes operator can be used to sync and decrypt secrets from a [password store (pass)](https://www.passwordstore.org/) Git repository.
--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 # Docker run the docker/PROJECT-directory.
 
-PROJECT="${1:-base_python}"
+PROJECT="${1:-"pass-operator"}"
 VERSION="${2:-0.1.0}"
 shift && shift
 
 
 # docker run -itd --name "$PROJECT" docker.ops.premiscale.com/"$PROJECT":"$VERSION" "$@"
 
-docker stop base_python && docker rm base_python
+docker stop pass-operator && docker rm pass-operator
 docker run -itd --name "${PROJECT}" -e DOPPLER_TOKEN="$(pass show premiscale/doppler/development-service-token)" docker-develop.ops.premiscale.com/"${PROJECT}":"${VERSION}" "$@"
 docker exec -it "${PROJECT}" /bin/bash
