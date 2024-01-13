726057cc4ada97704e3fc3c223c73af3dccff9f8
Emma Doyle <emma@sbevision.com>
2024-01-13 15:05:15 -0500

    ironing: standardize how env vars are read

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 # Start the pass operator after some initial SSH setup.
 
+if [ -n "$PASS_SSH_PRIVATE_KEY" ]; then
+    printf "ERROR: PASS_SSH_PRIVATE_KEY is not defined. Please provide a valid private SSH key.\\n" >&2
+    exit 1
+fi
+
 # Add private SSH key to the agent.
 eval "$(ssh-agent -s)"
-printf "%s" "$SSH_PRIVATE_KEY" > ~/.ssh/private-key
+printf "%s" "$PASS_SSH_PRIVATE_KEY" > ~/.ssh/private-key
 ssh-add ~/.ssh/private-key
 
 # Import gpg key.
--- src/passoperator/operator.py
+++ src/passoperator/operator.py
@@ -22,10 +22,22 @@ from src.passoperator.utils import LogLevel
 
 __version__ = meta.version('pass-operator')
 log = logging.getLogger(__name__)
-
-
-INTERVAL = int(os.getenv('PASSOPERATOR_INTERVAL') or 60)
 pass_git_repo: GitRepo
+
+
+# Environment variables to configure the operator's performance.
+OPERATOR_INTERVAL = int(os.getenv('OPERATOR_INTERVAL') or 60)
+OPERATOR_INITIAL_DELAY = int(os.getenv('OPERATOR_INITIAL_DELAY') or 3)
+OPERATOR_PRIORITY = int(os.getenv('OPERATOR_PRIORITY') or 100)
+OPERATOR_NAMESPACE = os.getenv('OPERATOR_NAMESPACE') or 'default'
+
+# Environment variables to configure pass.
+PASS_BINARY = os.getenv('PASS_BINARY') or '/usr/bin/pass'
+PASS_DIRECTORY = os.getenv('PASS_DIRECTORY') or '/opt/pass-operator/repo'
+PASS_GPG_KEY_ID = os.getenv('PASS_GPG_KEY_ID')
+PASS_GIT_URL = os.getenv('PASS_GIT_URL')
+PASS_GIT_BRANCH = os.getenv('PASS_GIT_BRANCH') or 'main'
+PASS_SSH_PRIVATE_KEY = os.getenv('PASS_SSH_PRIVATE_KEY')
 
 
 @cache
@@ -43,31 +55,9 @@ def read_manifest(path: str) -> Dict[str, str]:
         return yaml.safe_load(obj.read().rstrip())
 
 
-def get_current_namespace(namespace_path: str ='/var/run/secrets/kubernetes.io/serviceaccount/namespace') -> str:
-    """
-    Get the current namespace this operator is deployed in. Refactored from the following GitHub issue.
-
-        https://github.com/kubernetes-client/python/issues/363
-
-    Args:
-        namespace_path (str): Namespace path in the local filesystem. Defaults to '/var/run/secrets/kubernetes.io/serviceaccount/namespace'.
-
-    Returns:
-        str: the namespace this process is running within on K8s.
-    """
-    if os.path.exists(namespace_path):
-        with open(namespace_path) as f:
-            return f.read().strip()
-    try:
-        _, active_context = kubernetes.config.list_kube_config_contexts()
-        return active_context['context']['namespace']
-    except KeyError:
-        return 'default'
-
-
-@kopf.on.cleanup()
-def cleanup(**kwargs) -> None:
-    pass
+# @kopf.on.cleanup()
+# def cleanup(**kwargs) -> None:
+#     pass
 
 
 @kopf.on.startup()
@@ -77,60 +67,61 @@ def start(param: Any, retry: Any, started: Any, runtime: Any, logger: Any, memo:
     bring current state of those objects into alignment.
     """
     log.info(f'Starting operator version {__version__}')
+    pass_git_repo.git_clone()
     # print(param, retry, started, runtime, logger, memo, activity, settings)
 
 
-@kopf.on.update('PassSecret')
-def update(**kwargs: Any) -> None:
-    """
-    An update was received on the PassSecret object, so attempt to update the corresponding Secret.
-    """
+# @kopf.on.update('PassSecret')
+# def update(**kwargs: Any) -> None:
+#     """
+#     An update was received on the PassSecret object, so attempt to update the corresponding Secret.
+#     """
 
 
-@kopf.on.create('PassSecret')
-def create(**kwargs: Any) -> None:
-    """
-    Create a new Secret from a PassSecret manifest.
+# @kopf.on.create('PassSecret')
+# def create(**kwargs: Any) -> None:
+#     """
+#     Create a new Secret from a PassSecret manifest.
 
-    Args:
-        version (str): version of the agent.
+#     Args:
+#         version (str): version of the agent.
 
-    Returns:
-        None.
-    """
-
-
-@kopf.on.delete('PassSecret')
-def delete(**kwargs: Any) -> None:
-    """
-    Remove the secret from memory.
-
-    Args:
-        spec (str):
-    """
+#     Returns:
+#         None.
+#     """
 
 
-@kopf.timer('PassSecret', interval=INTERVAL, initial_delay=1)
+# @kopf.on.delete('PassSecret')
+# def delete(**kwargs: Any) -> None:
+#     """
+#     Remove the secret from memory.
+
+#     Args:
+#         spec (str):
+#     """
+
+
+@kopf.timer('PassSecret', interval=OPERATOR_INTERVAL, initial_delay=OPERATOR_INITIAL_DELAY)
 def reconciliation() -> None:
     """
-    Reconcile secrets across all namespaces and ensure they match the stored desired state.
+    Reconcile secrets across all namespaces and ensure they match the state of the PassSecrets.
     """
     pass_git_repo.git_pull()
 
 
-@kopf.on.probe(id='now')
-def get_current_timestamp(**kwargs) -> str:
-    return datetime.datetime.utcnow().isoformat()
+# @kopf.on.probe(id='now')
+# def get_current_timestamp(**kwargs) -> str:
+#     return datetime.datetime.utcnow().isoformat()
 
 
-@kopf.on.probe(id='status')
-def get_current_status(**kwargs) -> str:
-    return 'ok'
+# @kopf.on.probe(id='status')
+# def get_current_status(**kwargs) -> str:
+#     return 'ok'
 
 
 def main() -> None:
     """
-    Set up operator.
+    Set up this wrapping Python program with logging, etc.
     """
     parser = ArgumentParser(description=__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
 
@@ -146,7 +137,7 @@ def main() -> None:
 
     parser.add_argument(
         '--log-level', default='info', choices=list(LogLevel), type=LogLevel.from_string,
-        help='Set the logging level. Valid choices are info, debug, error, and warn (in any case).'
+        help='Set the logging level. Valid choices are debug, info, warn, and error.'
     )
 
     parser.add_argument(
@@ -154,41 +145,23 @@ def main() -> None:
         help='Log file location (if log-stdout is not provided).'
     )
 
-    parser.add_argument(
-        '--pass-binary', type=str, default='/usr/bin/pass',
-        help='Path to an alternate pass binary.'
-    )
-
-    parser.add_argument(
-        '--priority', type=int, default=100,
-        help='Operator priority.'
-    )
-
-    parser.add_argument(
-        '--pass-dir', type=str, default='/opt/pass-operator/repo',
-        help='Pass directory to clone into.'
-    )
-
-    parser.add_argument(
-        '--gpg-key-id', type=str,
-        help='Private GPG key ID to use with pass to decrypt secrets.'
-    )
-
-    parser.add_argument(
-        '--git-ssh-url', type=str,
-        help='Repository\'s git domain to clone.'
-    )
-
-    parser.add_argument(
-        '--git-branch', type=str, default='main',
-        help='Git branch to pull secrets from in repository.'
-    )
-
     args = parser.parse_args()
 
     if args.version:
         print(f'passoperator v{__version__}')
         sys.exit(0)
+
+    if not PASS_GPG_KEY_ID:
+        log.error(f'Must provide a valid GPG key ID (PASS_GPG_KEY_ID).')
+        sys.exit(1)
+
+    if not PASS_GIT_URL:
+        log.error(f'Must provide a valid git URL (PASS_GIT_URL).')
+        sys.exit(1)
+
+    if not PASS_SSH_PRIVATE_KEY:
+        log.error(f'Must provide a valid private SSH key to clone Git repo. (PASS_SSH_PRIVATE_KEY).')
+        sys.exit(1)
 
     # Configure logger
     if args.log_stdout:
@@ -225,9 +198,9 @@ def main() -> None:
         # paths: List[str],
         # modules: List[str],
         # peering_name: Optional[str]
-        priority=args.priority,
+        priority=OPERATOR_PRIORITY,
         standalone=True,
-        namespaces=get_current_namespace(),
+        namespaces=OPERATOR_NAMESPACE,
         clusterwide=False,
         # liveness_endpoint: Optional[str],
     )
