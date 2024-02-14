70b8adcaf276d61ba53ef525fe01d349bb12aaa8
Emma Doyle <emma@sbevision.com>
2024-02-14 09:39:11 -0500

    master:

--- src/operator/git.py
+++ src/operator/git.py
@@ -4,10 +4,12 @@ Methods to interact minimally with a Git repository.
 
 
 from git import Repo
+from git.exc import CommandError
 from time import sleep
 from src.operator import env
 
 import logging
+import sys
 
 
 log = logging.getLogger(__name__)
@@ -32,20 +34,29 @@ def clone() -> None:
     log.info(f'Successfully cloned repo {env["PASS_GIT_URL"]} to password store {env["PASS_DIRECTORY"]}')
 
 
-def pull(daemon: bool =False) -> None:
+def pull(daemon: bool =False, retry: bool =False) -> None:
     """
-    Blocking function that optionally runs 'git pull' in the cloned repository, repeatedly.
+    Blocking function that optionally runs 'git pull' in the cloned repository, repeatedly. This said, the
+    default behavior is to retry indefinitely until a 'git pull' succeeds.
 
     Args:
         daemon (bool): whether or not to loop on the user-specified OPERATOR_INTERVAL (default: False).
     """
-    if daemon:
-        while True:
+    tries = 0
+
+    while daemon or retry:
+        # Try to pull from the repository. If successful and daemon is not set, break from the loop.
+        # Otherwise, continue to try to pull from the repository on an interval.
+        try:
             log.info(f'Updating local password store at "{env["PASS_DIRECTORY"]}"')
             repo = Repo(env['PASS_DIRECTORY'])
             repo.remotes.origin.pull()
-            sleep(float(env['OPERATOR_INTERVAL']))
-    else:
-        log.info(f'Updating local password store at "{env["PASS_DIRECTORY"]}"')
-        repo = Repo(env['PASS_DIRECTORY'])
-        repo.remotes.origin.pull()
+            if daemon:
+                tries = 0
+                sleep(float(env['OPERATOR_INTERVAL']))
+        except CommandError as e:
+            log.error(f'Retry {tries} git pull: {e}')
+            tries += 1
+
+        if not daemon:
+            break
