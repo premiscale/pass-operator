20589c40708c8f25f5fcfe5fe599e2c2e39a987c
Emma Doyle <emma.ann.doyle@gmail.com>
2024-04-04 11:49:26 -0400

    Decouple from on-premise development cluster

--- scripts/minikube.sh
+++ scripts/minikube.sh
@@ -2,20 +2,40 @@
 # Manage a minikube cluster.
 
 
-minikube start \
-    -p pass-operator \
-    --cpus 4 \
-    --nodes 2 \
-    --memory 8192 \
-    --disk-size 30g \
-    --kubernetes-version v1.28.3 \
-    --extra-config=kubelet.runtime-request-timeout=40m \
-    --addons=ingress \
-    --addons=metallb \
-    --addons=metrics-server
+set -eo pipefail
+shopt -s nullglob
 
-minikube kubectl -p pass-operator get nodes
 
-minikube stop -p pass-operator
+if ! command -v minikube &> /dev/null; then
+    printf "ERROR: executable 'minikube' could not be found" >&2
+    exit 1
+fi
 
-minikube delete -p pass-operator
+
+if [ $1 != "start" ] || [ $1 != "stop" ] || [ $1 != "delete" ]; then
+    printf "ERROR: Usage: $0 [ start | stop | delete ]\\n" >&2
+    exit 1
+fi
+
+
+if [ "$1" == "start" ]; then
+    # --cpus 4 \
+    # --nodes 2 \
+    # --memory 4096 \
+    # --disk-size 30g \
+
+    minikube start \
+        -p pass-operator \
+        --kubernetes-version v1.28.3 \
+        --extra-config=kubelet.runtime-request-timeout=40m \
+        --addons=ingress \
+        --addons=metallb \
+        --addons=metrics-server
+
+    minikube kubectl -p pass-operator get nodes -o wide
+    minikube kubectl -p pass-operator get pods -A
+elif [ "$1" == "stop" ]; then
+    minikube stop -p pass-operator
+elif [ "$1" == "delete" ]; then
+    minikube delete -p pass-operator
+fi
