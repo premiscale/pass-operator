1beda78b50e6810e212e145dc462e4e2c919de65
Emma Doyle <emma.ann.doyle@gmail.com>
2024-05-28 13:16:48 -0400

    write-e2e-tests: run full test environment

--- src/test/e2e/lib.py
+++ src/test/e2e/lib.py
@@ -169,6 +169,7 @@ def delete_gpg_keypair(key_id: str, passphrase: str) -> None:
 
 
 def build_e2e_image(
+        registry: str = 'localhost:5000',
         tag: str = '0.0.1',
         pass_version: str = '1.7.4-5',
         tini_version: str = 'v0.19.0',
@@ -181,11 +182,11 @@ def build_e2e_image(
         int: The return code of the docker build or push command that failed, or 0 if both succeeded.
     """
     return run([
-        'docker', 'build', '-t', f'localhost:5000/pass-operator-e2e:{tag}', '-f', './src/test/Dockerfile.e2e', './src/test/',
+        'docker', 'build', '-t', f'{registry}/pass-operator-e2e:{tag}', '-f', './src/test/Dockerfile.e2e', './src/test/',
         '--build-arg', f'PASS_VERSION={pass_version}',
         '--build-arg', f'TINI_VERSION={tini_version}',
         '--build-arg', f'ARCHITECTURE={architecture}',
-    ]).returnCode or run(['docker', 'push', f'localhost:5000/pass-operator-e2e:{tag}']).returnCode
+    ]).returnCode or run(['docker', 'push', f'{registry}/pass-operator-e2e:{tag}']).returnCode
 
 
 def install_pass_operator_e2e(
@@ -198,7 +199,8 @@ def install_pass_operator_e2e(
         gpg_createSecret: bool = True,
         gpg_passphrase: str = '',
         git_branch: str = 'main',
-        image_tag: str = '0.0.1'
+        image_tag: str = '0.0.1',
+        registry: str = 'localhost:5000'
     ) -> int:
     """
     Install the e2e testing image in the cluster.
@@ -208,7 +210,7 @@ def install_pass_operator_e2e(
     """
     return run([
         'helm', 'upgrade', '--install', '--namespace', namespace, '--create-namespace', 'pass-operator-e2e', './helm/operator-e2e',
-            '--set', 'global.image.registry=localhost:5000',
+            '--set', f'global.image.registry={registry}',
             '--set', f'deployment.image.tag={image_tag}',
             '--set', f'operator.ssh.createSecret={str(ssh_createSecret).lower()}',
             '--set', f'operator.pass.storeSubPath={pass_storeSubPath}',
@@ -231,14 +233,17 @@ def uninstall_pass_operator_e2e(namespace: str = 'default') -> int:
     return run(['helm', 'uninstall', '--namespace', namespace, 'pass-operator-e2e']).returnCode
 
 
-def cleanup_e2e_image(tag: str = '0.0.1') -> int:
+def cleanup_e2e_image(
+        registry: str = 'localhost:5000',
+        tag: str = '0.0.1'
+    ) -> int:
     """
     Remove the e2e image.
 
     Returns:
         int: The return code of the docker rmi command.
     """
-    return run(['docker', 'rmi', f'localhost:5000/pass-operator-e2e:{tag}']).returnCode
+    return run(['docker', 'rmi', f'{registry}/pass-operator-e2e:{tag}']).returnCode
 
 
 def install_pass_operator_crds(namespace: str = 'default') -> int:
@@ -261,25 +266,31 @@ def uninstall_pass_operator_crds(namespace: str = 'default') -> int:
     return run(['helm', 'uninstall', '--namespace', namespace, 'pass-operator-crds']).returnCode
 
 
-def cleanup_operator_image(tag: str = '0.0.1') -> int:
+def cleanup_operator_image(
+        registry: str = 'localhost:5000',
+        tag: str = '0.0.1'
+    ) -> int:
     """
     Remove the operator image.
 
     Returns:
         int: The return code of the docker rmi command.
     """
-    return run(['docker', 'rmi', f'localhost:5000/pass-operator:{tag}']).returnCode
+    return run(['docker', 'rmi', f'{registry}/pass-operator:{tag}']).returnCode
 
 
-def build_operator_image(tag: str = '0.0.1') -> int:
+def build_operator_image(
+        registry: str = 'localhost:5000',
+        tag: str = '0.0.1'
+    ) -> int:
     """
     Build the local operator image.
 
     Returns:
         int: The return code of the docker build or push command that failed, or 0 if both succeeded.
     """
-    return run(['docker', 'build', '-t', f'localhost:5000/pass-operator:{tag}', '-f', './Dockerfile.develop', '.']).returnCode \
-        or run(['docker', 'push', f'localhost:5000/pass-operator:{tag}']).returnCode
+    return run(['docker', 'build', '-t', f'{registry}/pass-operator:{tag}', '-f', './Dockerfile.develop', '.']).returnCode \
+        or run(['docker', 'push', f'{registry}/pass-operator:{tag}']).returnCode
 
 
 def install_pass_operator(
@@ -294,7 +305,9 @@ def install_pass_operator(
         gpg_createSecret: bool = True,
         gpg_passphrase: str = '',
         git_branch: str = 'main',
-        image_tag: str = '0.0.1'
+        image_tag: str = '0.0.1',
+        registry: str = 'localhost:5000',
+        operator_interval: int = 60,
     ) -> int:
     """
     Install the operator in the cluster.
@@ -304,9 +317,9 @@ def install_pass_operator(
     """
     return run([
         'helm', 'upgrade', '--install', 'pass-operator', './helm/operator', '--namespace', namespace, '--create-namespace',
-            '--set', 'global.image.registry=localhost:5000',
+            '--set', f'global.image.registry={registry}',
             '--set', f'deployment.image.tag={image_tag}',
-            '--set', 'operator.interval=60',
+            '--set', f'operator.interval={operator_interval}',
             '--set', 'operator.initial_delay=1',
             '--set', f'operator.priority={priority}',
             '--set', f'operator.ssh.createSecret={str(ssh_createSecret).lower()}',
--- src/test/e2e/test_crd.py
+++ src/test/e2e/test_crd.py
@@ -16,6 +16,8 @@ from test.e2e.lib import (
     check_cluster_pod_status,
     generate_gpg_keypair,
     generate_ssh_keypair,
+    generate_unencrypted_crds,
+    cleanup_unencrypted_crds,
     # Docker
     build_operator_image,
     cleanup_operator_image,
@@ -41,89 +43,112 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-# # Uninstalling prior Helm chart installations.
-# uninstall_pass_operator(namespace='pass-operator')
-# uninstall_pass_operator_e2e(namespace='pass-operator')
-# uninstall_pass_operator_crds(namespace='pass-operator')
-
 # Generate GPG and SSH keypairs for use in testing.
-log.info('Generating GPG and SSH keypairs for testing.')
-gpg_passphrase = random_secret()
-gpg_public_key, gpg_private_key, gpg_fingerprint = generate_gpg_keypair(
-    passphrase=gpg_passphrase,
-    delete_from_keyring=True
-)
-ssh_public_key, ssh_private_key = generate_ssh_keypair()
-
-log.info('Building e2e image')
-# e2e artifacts that the operator depends on to run.
-build_e2e_image()
-
-log.info('Installing pass-operator-e2e')
-install_pass_operator_e2e(
-    ssh_value=ssh_public_key,
-    gpg_value=gpg_public_key,
-    gpg_key_id=gpg_fingerprint,
-    namespace='pass-operator',
-    ssh_createSecret=True,
-    pass_storeSubPath='repo',
-    gpg_createSecret=True,
-    gpg_passphrase=gpg_passphrase,
-    git_branch='main'
-)
-
-log.info('Installing pass-operator crds')
-
-# Build and install operator artifacts.
-install_pass_operator_crds(namespace='pass-operator')
-
-log.info('Building operator image')
-build_operator_image()
-
-log.info('Installing pass-operator')
-install_pass_operator(
-    ssh_value=ssh_private_key,
-    gpg_value=gpg_private_key,
-    gpg_key_id=gpg_fingerprint,
-    namespace='pass-operator',
-    ssh_createSecret=True,
-    pass_storeSubPath='repo',
-    gpg_createSecret=True,
-    gpg_passphrase=gpg_passphrase,
-    git_url='root@pass-operator-e2e:/opt/operator/repo.git',
-    git_branch='main'
-)
-
-cleanup_e2e_image()
-cleanup_operator_image()
 
 config.load_kube_config(
     context='pass-operator'
 )
-
-passsecret_data_singular = load_data('test_singular_data')
-passsecret_data_zero = load_data('test_zero_data')
-passsecret_data_multiple = load_data('test_multiple_data')
-passsecret_data_singular_immutable = load_data('test_singular_data_immutable')
-passsecret_data_singular_different_managed_secret_name = load_data('test_singular_data_different_managed_secret_name')
-passsecret_singular_collision_1 = load_data('test_singular_data_collision_1')
-passsecret_singular_collision_2 = load_data('test_singular_data_collision_2')
 
 
 class PassSecretE2E(TestCase):
     """
     Methods for testing the Kubernetes operator end-to-end.
     """
+    passsecret_data_singular: dict
+    decrypted_passsecret_data_singular: dict
+
+    passsecret_data_zero: dict
+    decrypted_passsecret_data_zero: dict
+
+    passsecret_data_multiple: dict
+    decrypted_passsecret_data_multiple: dict
+
+    passsecret_data_singular_immutable: dict
+    decrypted_passsecret_data_singular_immutable: dict
+
+    passsecret_data_singular_different_managed_secret_name: dict
+    decrypted_passsecret_data_singular_different_managed_secret_name: dict
+
+    passsecret_singular_collision_1: dict
+    decrypted_passsecret_singular_collision_1: dict
+
+    passsecret_singular_collision_2: dict
+    decrypted_passsecret_singular_collision_2: dict
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Common steps to run before all tests.
+        """
+        gpg_passphrase = random_secret()
+
+        ## Generate GPG and SSH keypairs for use in testing.
+        gpg_public_key, gpg_private_key, gpg_fingerprint = generate_gpg_keypair(
+            passphrase=gpg_passphrase,
+            delete_from_keyring=True
+        )
+        ssh_public_key, ssh_private_key = generate_ssh_keypair()
+
+        ## Build the e2e image and install the pass-operator-e2e Helm chart.
+        build_e2e_image()
+        install_pass_operator_e2e(
+            ssh_value=ssh_public_key,
+            gpg_value=gpg_public_key,
+            gpg_key_id=gpg_fingerprint,
+            namespace='pass-operator',
+            ssh_createSecret=True,
+            pass_storeSubPath='repo',
+            gpg_createSecret=True,
+            gpg_passphrase=gpg_passphrase,
+            git_branch='main'
+        )
+
+        ## Install the pass-operator Helm chart and CRDs.
+        install_pass_operator_crds(namespace='pass-operator')
+        build_operator_image()
+        install_pass_operator(
+            ssh_value=ssh_private_key,
+            gpg_value=gpg_private_key,
+            gpg_key_id=gpg_fingerprint,
+            namespace='pass-operator',
+            ssh_createSecret=True,
+            pass_storeSubPath='repo',
+            gpg_createSecret=True,
+            gpg_passphrase=gpg_passphrase,
+            git_url='root@pass-operator-e2e:/opt/operator/repo.git',
+            git_branch='main'
+        )
+
+        generate_unencrypted_crds()
+
+        cls.passsecret_data_singular = load_data('test_singular_data')
+        cls.decrypted_passsecret_data_singular = load_data('test_singular_data.unencrypted')
+
+        cls.passsecret_data_zero = load_data('test_zero_data')
+        cls.decrypted_passsecret_data_zero = load_data('test_zero_data.unencrypted')
+
+        cls.passsecret_data_multiple = load_data('test_multiple_data')
+        cls.decrypted_passsecret_data_multiple = load_data('test_multiple_data.unencrypted')
+
+        cls.passsecret_data_singular_immutable = load_data('test_singular_data_immutable')
+        cls.decrypted_passsecret_data_singular_immutable = load_data('test_singular_data_immutable.unencrypted')
+
+        cls.passsecret_data_singular_different_managed_secret_name = load_data('test_singular_data_different_managed_secret_name')
+        cls.decrypted_passsecret_data_singular_different_managed_secret_name = load_data('test_singular_data_different_managed_secret_name.unencrypted')
+
+        cls.passsecret_singular_collision_1 = load_data('test_singular_data_collision_1')
+        cls.decrypted_passsecret_singular_collision_1 = load_data('test_singular_data_collision_1.unencrypted')
+
+        cls.passsecret_singular_collision_2 = load_data('test_singular_data_collision_2')
+        cls.decrypted_passsecret_singular_collision_2 = load_data('test_singular_data_collision_2.unencrypted')
 
     def setUp(self) -> None:
         """
-        Create a PassSecret instance for use in testing.
+        Common steps to run before every test.
         """
 
         # Wait for all pods in the cluster to be ready.
         check_cluster_pod_status()
-
-        return super().setUp()
 
     # def tearDown(self) -> None:
     #     return super().tearDown()
@@ -132,6 +157,16 @@ class PassSecretE2E(TestCase):
         """
         Test that the operator is running as intended in the cluster.
         """
+        v1 = client.CustomObjectsApi()
+
+        # Create a namespaced PassSecret object, then vet that the managed secret both exists and contains the proper data (i.e., the operator did its job).
+        v1.create_namespaced_custom_object(
+            group='secrets.premiscale.com',
+            version='v1alpha1',
+            namespace='pass-operator',
+            plural='passsecrets',
+            body=self.passsecret_data_singular
+        )
 
     def test_operator_zero_data(self) -> None:
         """
@@ -162,3 +197,18 @@ class PassSecretE2E(TestCase):
         """
         Test that the operator is running as intended in the cluster.
         """
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """
+        Common steps to run after all tests.
+        """
+
+        ## Reset the cluster and clean up resources.
+        cleanup_e2e_image()
+        cleanup_operator_image()
+        cleanup_unencrypted_crds()
+
+        uninstall_pass_operator(namespace='pass-operator')
+        uninstall_pass_operator_e2e(namespace='pass-operator')
+        uninstall_pass_operator_crds(namespace='pass-operator')
